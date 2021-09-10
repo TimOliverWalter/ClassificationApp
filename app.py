91fa706140bdd67311ccd8786b5f3d182f2c5e14
Tim Oliver Walter <timwalter94@web.de>
2021-09-10 23:15:38 +0200

    random forest added

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.df = pd.read_csv("data/churn_modelling.csv")
+df = df.drop(["RowNumber", "CustomerId", "Surname"], axis=1)
+df = df.replace(
+    {"Geography": {"Germany": 0,
+                   "France": 1,
+                   "Spain": 2},
+     "Gender": {"Female": 0,
+                "Male": 1}
+     }
+)
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+X = df.drop(["Exited"], axis=1)
+y = df["Exited"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+clf = RandomForestClassifier(random_state=10)
+clf.fit(X_train, y_train)
+y_hat = clf.predict(X_test)
+
+result = pd.DataFrame(y_hat, columns=["Exited_Yhat"])
+y_test = y_test.reset_index(drop=True)
+
+result["Exited"] = y_test
+
+print(result)
+print(y_test)
+print(X_test)
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -27,7 +46,7 @@ app.layout = html.Div(children=[
 
     dcc.Graph(
         id='example-graph',
-        figure=fig
+        figure=None
     )
 ])
 

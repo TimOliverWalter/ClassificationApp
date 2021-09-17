a4b1db4e3b6292909c60c32836b275afedb40036
Tim Oliver Walter <timwalter94@web.de>
2021-09-17 21:28:36 +0200

    new tabstyle

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import base64
+import io
 
 import dash
 import dash_bootstrap_components as dbc
@@ -23,211 +25,102 @@ from sklearn.metrics import (
     accuracy_score
 )
 
+page2 = html.Div([
+    html.H5(
+        "Text"
+    )
+])
+external_scripts = ["/assets/style.css"]
+app = dash.Dash()
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-app.layout = dbc.Container(
+app.layout = html.Div(
     [
-        html.H1("Dynamically rendered tab content"),
-        html.Hr(),
-        dbc.Tabs(
+        dcc.Tabs(
             id="tabs",
-            children=
-            [
-                dbc.Tab(
-                    label="Sample data",
-                    tab_id="sample-data"
+            children=[
+                dcc.Tab(
+                    id="page-one",
+                    label="New Data",
+                    children=[
+                        html.Div(
+                            [
+                                dcc.Upload(
+                                    id="upload-page-one",
+                                    children=html.Div(
+                                        [
+                                            "Drag and Drop or ",
+                                            html.A(
+                                                html.B(
+                                                    html.I(
+                                                        "Select your Excel File"
+                                                    )
+                                                )
+                                            )
+                                        ]
+                                    ),
+                                    style={
+                                        "width": "99%",
+                                        "height": "60px",
+                                        "lineHeight": "60px",
+                                        "borderWidth": "1px",
+                                        "borderStyle": "dashed",
+                                        "borderRadius": "5px",
+                                        "textAlign": "center",
+                                        "margin": "auto",
+                                        "margin-top": "10px",
+                                        "margin-bottom": "10px"
+                                    }
+                                ),
+                                html.Div(
+                                    id="output-page-one",
+                                    children=[]
+                                )
+                            ]
+                        )
+                    ]
                 ),
-                dbc.Tab(
-                    label="New data",
-                    tab_id="new-data"
-                ),
-            ],
-            active_tab="sample-data",
-        ),
-        html.Div(
-            id="tab-content"
-        ),
+                dcc.Tab(
+                    id="page-two",
+                    label="test",
+                    children=[
+                        page2
+                    ]
+                )
+            ]
+        )
     ]
 )
 
 
 @app.callback(
-    Output("tab-content", "children"),
-    Input("tabs", "active_tab")
+    Output("output-page-one", "children"),
+    Input("upload-page-one", "contents")
 )
-def render_tab_content(active_tab):
-    df = pd.read_csv("data/churn_modelling.csv")
-    df = df.drop(["RowNumber", "CustomerId", "Surname"], axis=1)
-    df = df.replace(
-        {"Geography": {"Germany": 0,
-                       "France": 1,
-                       "Spain": 2},
-         "Gender": {"Female": 0,
-                    "Male": 1}
-         }
-    )
+def update_page_one(contents):
+    if contents is not None:
+        content_type, content_string = contents.split(",")
+        decoded = base64.b64decode(content_string)
 
-    X = df.drop(["Exited"], axis=1)
-    y = df["Exited"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+        df = pd.read_csv(
+            io.BytesIO(decoded)
+        )
 
-    gbc = GradientBoostingClassifier()
-    gbc.fit(X_train, y_train)
-    y_pred_gbc = gbc.predict(X_test)
-
-    cm = confusion_matrix(y_test, y_pred_gbc)
-    fig = px.imshow(
-        cm,
-        labels=dict(x="Predicted class", y="Actual class"),
-        x=["P", "N"],
-        y=["P", "N"]
-    )
-    fig.update_xaxes(side="top")
-
-    accuracy = round(accuracy_score(y_test, y_pred_gbc), 4)
-    f1 = round(f1_score(y_test, y_pred_gbc), 4)
-    precision = round(precision_score(y_test, y_pred_gbc), 4)
-    recall = round(recall_score(y_test, y_pred_gbc), 4)
-
-    fig1 = go.Figure(
-        data=[
-            go.Table(
-                header=dict(
-                    values=["", "Metrics"]
+        page = html.Div([
+            html.H5(
+                "Text"
+            ),
+            #dcc.Graph(figure=px.histogram(df, x="Age", color="Exited")),
+            dash_table.DataTable(
+                data=df.to_dict(
+                    orient="records"
                 ),
-                cells=dict(
-                    values=[
-                        ["Accuracy", "F1", "Precision", "Recall"],
-                        [accuracy, f1, precision, recall]
-                    ]
-                )
+                columns=[
+                    {"name": i, "id": i} for i in df.columns
+                ],
+                page_size=5
             )
-        ]
-    )
-
-    """
-    2nd option 
-    
-    table_header = [
-        html.Thead(html.Tr([html.Th("metrics")]))
-    ]
-    row1 = html.Tr([html.Td("Arthur"), html.Td("Dent")])
-    table_body = [html.Tbody([row1])]
-    table = dbc.Table(table_header + table_body, bordered=True)
-    """
-
-    if active_tab is not None:
-        if active_tab == "sample-data":
-            return dbc.Row(
-                html.Div(
-                    [
-                        html.H5("Exploratory Data Analysis (EDA)"),
-                        dbc.Row(
-                            [
-                                dbc.Col(
-                                    daq.LEDDisplay(
-                                        label="Records",
-                                        value=len(df.index),
-                                        backgroundColor="#FF5E5E"
-                                    ),
-                                    width=3
-                                ),
-                                dbc.Col(
-                                    daq.LEDDisplay(
-                                        label="Train",
-                                        value=len(X_train.index),
-                                        backgroundColor="#FF5E5E"
-                                    ),
-                                    width=3
-                                ),
-                                dbc.Col(
-                                    daq.LEDDisplay(
-                                        label="Test",
-                                        value=len(X_test.index),
-                                        backgroundColor="#FF5E5E"
-                                    ),
-                                    width=3
-                                )
-                            ]
-                        ),
-                        dbc.Row(
-                            [
-                                dbc.Col(
-                                    dcc.Graph(
-                                        figure=fig
-                                    ),
-                                    width=6
-                                ),
-                                dbc.Col(
-                                    dcc.Graph(
-                                        figure=fig1
-                                    ),
-                                    width=6
-                                )
-                            ]
-                        ),
-                        dbc.Row(
-                            dash_table.DataTable(
-                                id="table",
-                                columns=[{"name": i, "id": i} for i in df.columns],
-                                data=df.to_dict("records"),
-                                page_size=5
-                            )
-                        )
-                    ]
-                )
-            )
-        elif active_tab == "new-data":
-            return dbc.Row(
-                [
-                    dbc.Row(
-                        dcc.Upload(
-                            id="upload-data",
-                            children=html.Div([
-                                "Drag and Drop or ",
-                                html.A("Select Files")
-                            ]),
-                            style={
-                                "width": "99%",
-                                "height": "60px",
-                                "lineHeight": "60px",
-                                "borderWidth": "1px",
-                                "borderStyle": "dashed",
-                                "borderRadius": "5px",
-                                "textAlign": "center",
-                                "margin": "10px"
-                            },
-                            multiple=False
-                        )
-                    ),
-                    dbc.Row(
-                        [
-                            dbc.Col(
-                                dcc.Graph(
-                                    figure=px.histogram(
-                                        df,
-                                        x="Age",
-                                        color="Exited"
-                                    )
-                                ),
-                                width=6
-                            ),
-                            dbc.Col(
-                                dcc.Graph(
-                                    figure=px.histogram(
-                                        df,
-                                        x="Age",
-                                        color="Exited"
-                                    )
-                                ),
-                                width=6
-                            )
-                        ]
-                    )
-                ]
-            )
-    return "No tab selected"
+        ])
+        return page
 
 
 if __name__ == "__main__":

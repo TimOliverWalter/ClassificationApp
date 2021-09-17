48e036a8dac34a11f58ca5406945319c6e946068
Tim Oliver Walter <timwalter94@web.de>
2021-09-17 22:37:42 +0200

    Heart failure tab created

--- app.py
+++ app.py
@@ -25,10 +25,73 @@ from sklearn.metrics import (
     accuracy_score
 )
 
+data = pd.read_csv("data/heart_failure.csv")
+fig = px.histogram(data, x="Age", color="Sex", pattern_shape="HeartDisease")
+
+encod_cols = ["Sex", "ChestPainType", "RestingECG", "ExerciseAngina", "ST_Slope"]
+dummy = pd.get_dummies(data[encod_cols], prefix=encod_cols, dtype=int)
+data = pd.merge(
+    left=data,
+    right=dummy,
+    left_index=True,
+    right_index=True,
+)
+data = data.drop(encod_cols, axis=1)
+
+X = data.drop(["HeartDisease"], axis=1)
+y = data["HeartDisease"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+gbc = GradientBoostingClassifier()
+gbc.fit(X_train, y_train)
+y_pred = gbc.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+fig1 = px.imshow(
+    cm,
+    labels=dict(x="Predicted class", y="Actual class"),
+    x=["P", "N"],
+    y=["P", "N"]
+)
+fig1.update_xaxes(side="top")
+
+accuracy = round(accuracy_score(y_test, y_pred), 4)
+f1 = round(f1_score(y_test, y_pred), 4)
+precision = round(precision_score(y_test, y_pred), 4)
+recall = round(recall_score(y_test, y_pred), 4)
+
+fig2 = go.Figure(
+    data=[
+        go.Table(
+            header=dict(
+                values=["", "Metrics"]
+            ),
+            cells=dict(
+                values=[
+                    ["Accuracy", "F1", "Precision", "Recall"],
+                    [accuracy, f1, precision, recall]
+                ]
+            )
+        )
+    ]
+)
+
 page2 = html.Div([
     html.H5(
         "Text"
-    )
+    ),
+    dash_table.DataTable(
+        data=data.to_dict(
+            orient="records"
+        ),
+        columns=[
+            {"name": i, "id": i} for i in data.columns
+        ],
+        page_size=5
+    ),
+    dcc.Graph(figure=fig),
+    dcc.Graph(figure=fig1),
+    dcc.Graph(figure=fig2)
 ])
 external_scripts = ["/assets/style.css"]
 app = dash.Dash()
@@ -52,7 +115,7 @@ app.layout = html.Div(
                                             html.A(
                                                 html.B(
                                                     html.I(
-                                                        "Select your Excel File"
+                                                        "Select the Data"
                                                     )
                                                 )
                                             )
@@ -69,7 +132,8 @@ app.layout = html.Div(
                                         "margin": "auto",
                                         "margin-top": "10px",
                                         "margin-bottom": "10px"
-                                    }
+                                    },
+                                    multiple=False
                                 ),
                                 html.Div(
                                     id="output-page-one",
@@ -81,7 +145,7 @@ app.layout = html.Div(
                 ),
                 dcc.Tab(
                     id="page-two",
-                    label="test",
+                    label="Heart Failure Data",
                     children=[
                         page2
                     ]
@@ -104,12 +168,15 @@ def update_page_one(contents):
         df = pd.read_csv(
             io.BytesIO(decoded)
         )
+        X = df.drop(["Exited"], axis=1)
+        y = df["Exited"]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
         page = html.Div([
             html.H5(
                 "Text"
             ),
-            #dcc.Graph(figure=px.histogram(df, x="Age", color="Exited")),
+            # dcc.Graph(figure=px.histogram(df, x="Age", color="Exited")),
             dash_table.DataTable(
                 data=df.to_dict(
                     orient="records"
@@ -118,7 +185,23 @@ def update_page_one(contents):
                     {"name": i, "id": i} for i in df.columns
                 ],
                 page_size=5
+            ),
+            daq.LEDDisplay(
+                label="Records",
+                value=len(df.index),
+                backgroundColor="#FF5E5E"
+            ),
+            daq.LEDDisplay(
+                label="Train",
+                value=len(X_train.index),
+                backgroundColor="#FF5E5E"
+            ),
+            daq.LEDDisplay(
+                label="Test",
+                value=len(X_test.index),
+                backgroundColor="#FF5E5E"
             )
+
         ])
         return page
 

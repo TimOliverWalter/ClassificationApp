e4af0dbd9bbf2ef9eef968ede2d62b1844202cd5
Tim Oliver Walter <timwalter94@web.de>
2021-09-20 21:27:39 +0200

    code style

--- app.py
+++ app.py
@@ -2,101 +2,19 @@ import pandas as pd
 import base64
 import io
 
-from pages import overview, classifier_selection
+from pages import (
+    overview,
+    classifier_selection,
+    example_data
+)
 
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import dash_table
-import dash_daq as daq
 
-import plotly.express as px
-import plotly.graph_objs as go
-
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import (
-    GradientBoostingClassifier
-)
-from sklearn.metrics import (
-    confusion_matrix,
-    f1_score,
-    precision_score,
-    recall_score,
-    accuracy_score
-)
-
-data = pd.read_csv("data/heart_failure.csv")
-fig = px.histogram(data, x="Age", color="Sex", pattern_shape="HeartDisease")
-
-encod_cols = ["Sex", "ChestPainType", "RestingECG", "ExerciseAngina", "ST_Slope"]
-dummy = pd.get_dummies(data[encod_cols], prefix=encod_cols, dtype=int)
-data = pd.merge(
-    left=data,
-    right=dummy,
-    left_index=True,
-    right_index=True,
-)
-data = data.drop(encod_cols, axis=1)
-
-X = data.drop(["HeartDisease"], axis=1)
-y = data["HeartDisease"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-gbc = GradientBoostingClassifier()
-gbc.fit(X_train, y_train)
-y_pred = gbc.predict(X_test)
-
-cm = confusion_matrix(y_test, y_pred)
-fig1 = px.imshow(
-    cm,
-    labels=dict(x="Predicted class", y="Actual class"),
-    x=["P", "N"],
-    y=["P", "N"]
-)
-fig1.update_xaxes(side="top")
-
-accuracy = round(accuracy_score(y_test, y_pred), 4)
-f1 = round(f1_score(y_test, y_pred), 4)
-precision = round(precision_score(y_test, y_pred), 4)
-recall = round(recall_score(y_test, y_pred), 4)
-
-fig2 = go.Figure(
-    data=[
-        go.Table(
-            header=dict(
-                values=["", "Metrics"]
-            ),
-            cells=dict(
-                values=[
-                    ["Accuracy", "F1", "Precision", "Recall"],
-                    [accuracy, f1, precision, recall]
-                ]
-            )
-        )
-    ]
-)
-
-page2 = html.Div([
-    html.H5(
-        "Text"
-    ),
-    dash_table.DataTable(
-        data=data.to_dict(
-            orient="records"
-        ),
-        columns=[
-            {"name": i, "id": i} for i in data.columns
-        ],
-        page_size=5
-    ),
-    dcc.Graph(figure=fig),
-    dcc.Graph(figure=fig1),
-    dcc.Graph(figure=fig2)
-])
-external_scripts = ["/assets/style.css"]
-app = dash.Dash()
+app = dash.Dash(external_stylesheets=["/assets/style.css"])
 
 app.layout = html.Div(
     [
@@ -149,7 +67,7 @@ app.layout = html.Div(
                     id="page-two",
                     label="Heart Failure Data",
                     children=[
-                        page2
+                        example_data.create_layout()
                     ]
                 ),
                 dcc.Tab(
@@ -168,7 +86,9 @@ app.layout = html.Div(
                 )
             ]
         ),
-        dcc.Store(id="intermediate-value")
+        dcc.Store(
+            id="intermediate-value"
+        )
     ]
 )
 
@@ -178,15 +98,17 @@ app.layout = html.Div(
     Input("upload-page-one", "contents")
 )
 def parse_data(contents):
-    df =pd.DataFrame()
+    df = pd.DataFrame()
     if contents is not None:
         content_type, content_string = contents.split(",")
         decoded = base64.b64decode(content_string)
-
-        df = pd.read_csv(
-            io.BytesIO(decoded)
-        )
-        return df.to_dict(orient="records")
+        try:
+            df = pd.read_csv(
+                io.BytesIO(decoded)
+            )
+            return df.to_dict(orient="records")
+        except Exception as e:
+            print(e)
     return df.to_dict(orient="records")
 
 
@@ -195,12 +117,9 @@ def parse_data(contents):
     Input("intermediate-value", "data")
 )
 def update_tab_one(dataset):
-    print(len(dataset))
-    if len(dataset) >0:
+    # first check if the dataset is not empty
+    if len(dataset) > 0:
         df = pd.DataFrame.from_dict(dataset)
-        X = df.drop(["Exited"], axis=1)
-        y = df["Exited"]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
         missing_values_count = df.isnull().sum().to_frame()
         describe = df.describe(include="all")
@@ -228,10 +147,12 @@ def update_tab_one(dataset):
     Input("intermediate-value", "data")
 )
 def update_tab_dree(dataset):
-    df = pd.DataFrame.from_dict(dataset)
-    classifier_tab = classifier_selection.create_layout(df=df)
+    if len(dataset) > 0:
+        df = pd.DataFrame.from_dict(dataset)
+        classifier_tab = classifier_selection.create_layout(df=df)
 
-    return classifier_tab
+        return classifier_tab
+    return html.Div()
 
 
 if __name__ == "__main__":
--- pages/overview.py
+++ pages/overview.py
@@ -1,59 +1,46 @@
 import dash_html_components as html
 import dash_table
-import dash_daq as daq
 
 
 def create_layout(df, describe):
-    return html.Div([
-        html.Div([
-            html.H6(
-                "Dataset overview"
-            ),
-            dash_table.DataTable(
-                data=df.to_dict(
-                    orient="records"
-                ),
-                columns=[
-                    {"name": i, "id": i} for i in df.columns
-                ],
-                page_size=5
-            )]),
-
-        # dcc.Graph(figure=px.histogram(df, x="Age", color="Exited")),
-
-        html.Div([
-            html.Div([
-                html.H6("Stats"),
-                dash_table.DataTable(
-                    data=describe.to_dict(
-                        orient="records"
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.H6(
+                        "Dataset overview"
                     ),
-                    columns=[
-                        {"name": i, "id": i} for i in describe.columns
-                    ], style_table={"minWidth": "100%"}
-                ),
+                    dash_table.DataTable(
+                        data=df.to_dict(
+                            orient="records"
+                        ),
+                        columns=[
+                            {"name": i, "id": i} for i in df.columns
+                        ],
+                        page_size=5
+                    )
+                ]
+            ),
+            html.Div(
+                [
+                    html.Div(
+                        [
+                            html.H6("Descriptive statistics"),
+                            dash_table.DataTable(
+                                data=describe.to_dict(
+                                    orient="records"
+                                ),
+                                columns=[
+                                    {"name": i, "id": i} for i in describe.columns
+                                ]
+                            ),
 
-            ], style={"display": "inline-block"}),
-        ]),
-        html.Div([
-            html.Div(
-                daq.LEDDisplay(
-                    label="Records",
-                    value=len(df.index),
-                    backgroundColor="#FF5E5E"
-                ), style={"display": "inline-block"}),
-            html.Div(
-                daq.LEDDisplay(
-                    label="Records",
-                    value=len(df.index),
-                    backgroundColor="#FF5E5E"
-                ), style={"display": "inline-block"}),
-            html.Div(
-                daq.LEDDisplay(
-                    label="Records",
-                    value=len(df.index),
-                    backgroundColor="#FF5E5E"
-                ), style={"display": "inline-block", "margin-left": "10px"}),
-        ])
+                        ], style={
+                            "display": "inline-block"
+                        }
+                    ),
+                ]
+            )
 
-    ])
+        ]
+    )
